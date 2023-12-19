2f849002416cf79682a178b243afbd3a0a0fbca0
Lerr1uqs <624299960@qq.com>
2023-12-19 18:41:40 +0800

    add needed member for StateWindow

--- src/assistant/statewindow.py
+++ src/assistant/statewindow.py
@@ -5,9 +5,21 @@ state window for execution display in terminal
 class StateWindow:
 
     def __init__(self) -> None:
+        # processing time
         self.create_time
         self.last_new_path
-        self.last_vuln_found # 
+        self.last_vuln_found # TODO: unique
+        # state's status
+        self.total_state_count
+        # fuzzing status
+        self.current_fuzzing_func
+        # coverage
+        self.coverage_rate # TODO: query observer
+        # vulns found
+
+        # device status
+        self.cpu_utilization
+
         pass
         
     
