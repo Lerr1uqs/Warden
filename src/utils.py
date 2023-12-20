01bc054c4b847795172c94376c641379a1c67437
Lerr1uqs <624299960@qq.com>
2023-12-20 18:08:16 +0800

    remove unnecessary lib

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,6 @@ from loguru import logger
 from typing import (Callable, Optional, TypeVar, List, Tuple, Type, Dict, Any, Union, Set, Generic, Sequence)
 import claripy
 import numbers
-from pydantic import BaseModel
 CONCRETE = numbers.Number# TODO
 # internal
 # from disassembler import (SolidityBinary)
