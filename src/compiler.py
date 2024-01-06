34c43ce8da72eef634210457e8684637e642502f
Lerr1uqs <624299960@qq.com>
2024-01-07 00:41:25 +0800

    complete the dfa partically

--- src/compiler.py
+++ src/compiler.py
@@ -3,22 +3,44 @@ solidity compiler, based on local solc
 '''
 import subprocess
 import shutil
+import solcx
 import json
 import os
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from binascii    import unhexlify
 from pathlib     import Path
 from web3        import Web3
 from utils       import *
 
+import networkx as nx
+
+def bfs(root: dict) -> List[str]:
+
+    queue = deque([root])
+    res   = []
+
+    while len(queue) != 0:
+        e = queue.popleft()
+
+        if e.get("nodeType") and e["nodeType"] == "Identifier":
+            res.append(e["name"])
+
+        else:
+            for k, v in e.items():
+                if type(v) == dict:
+                    queue.append(v)
+
+    return res
+
 
 
 class Artifact:
-    def __init__(self, rtbc: str, initbc: str, json_obj: List[Dict]) -> None:
-        self.rtbc = rtbc
+    def __init__(self, rtbc: str, initbc: str, json_obj: List[Dict], ast: Dict) -> None:
+        self.rtbc   = rtbc
         self.initbc = initbc
-        self.abi = json_obj
+        self.abi    = json_obj
+        self.ast    = ast
         '''
         [
             {
@@ -66,6 +88,7 @@ class Artifact:
         # contain constructor
         self.func_input_types: Dict[str, List] = defaultdict(lambda: [])
 
+        # handle the function input info
         for func in json_obj:
 
             if func["type"] == "constructor":
@@ -106,6 +129,86 @@ class Artifact:
                 self.signatures[fname] = hash
                 self.funcs[fname] = func
                 self.func_input_types[fname] = function_input_types
+
+        # handle ast info
+        # REF: assets/ast.json
+        class NodeType:
+            def __init__(self) -> None:
+                self
+                pass
+        # the variable declared in the storage region
+        # NOTE: 目前简单采用同名去辨别变量 更复杂的情况需要区别重命名 暂时不做处理
+        storage_variable_names: List[int] = []
+
+        # dfa[fname]["w/r"] = List[var_name]
+        # TODO: 转换为set
+        dfa: Dict[str, Dict[str, Set[str]]] = defaultdict(lambda: defaultdict(lambda: set()))
+
+        for node in ast["nodes"]:
+            if node["nodeType"] == "ContractDefinition":
+                nodes = node["nodes"]
+                break
+
+        for node in nodes:
+            print(node["nodeType"])
+            
+            if node["nodeType"] == "VariableDeclaration":
+                storage_variable_names.append(node["name"])
+                print(f'[DBG] storage_variable_names append {node["name"]}')
+                continue
+
+            if node["nodeType"] == "FunctionDefinition":
+
+                fname = node["name"]
+                # TEMP: skip constructor currently
+                if node["kind"] == "constructor":
+                    continue
+
+                # TODO: 假定statement是一行的内容
+                # leftExpression 和 rightExpression是嵌套的 不过做DFA只需要
+                # TODO: 不知道为什么 selfdestruct也算Identifier 先不管了
+                for statement in node["body"]["statements"]:
+                    if statement["nodeType"] == "ExpressionStatement":
+                        expression = statement["expression"]
+
+                        if expression["nodeType"] == "Assignment":
+                            dfa[fname]["w"].add(expression["leftHandSide"]["name"])
+                            print(f'[DBG] dfa[{fname}][\"w\"] append {expression["leftHandSide"]["name"]}')
+                            dfa[fname]["r"].update(bfs(expression["rightHandSide"]))
+                        else:
+                            dfa[fname]["r"].update(bfs(expression))
+
+                    else:
+                        dfa[fname]["r"].update(bfs(statement))
+                
+
+        self.dfa = dfa
+
+        graph = nx.DiGraph()
+        flist = []
+
+        # all function nodes
+        for fname in dfa.keys():
+            graph.add_node(fname)
+            flist.append(fname)
+
+        # build the topology graph
+        for fname in flist:
+            for var in dfa[fname]["w"]:
+                for otherf in flist:
+                    if otherf == fname:
+                        continue
+                    if var in dfa[otherf]["r"]:
+                        # means F(fname) write a var which F(otherf) read
+                        graph.add_edge(fname, otherf) # fname -> otherf
+
+        self.ftopo_graph = graph # function topology graph
+
+        
+
+
+
+
 
 class Compiler:
 
@@ -166,6 +269,7 @@ class Compiler:
                     '--opcodes', 
                     '--abi', 
                     '--overwrite',
+                    '--ast-compact-json',
                     os.path.join(con_path, file), # e.g. contracts/selfdestruct.sol
                     '-o', 
                     folder_path
@@ -199,13 +303,16 @@ class Compiler:
 
                 with open(cnwp + ".abi", "r") as f:
                     abi = json.load(f)
-                
+
+                ast_path = folder_path + "/" + file.split(".")[0]
+                with open(ast_path + ".sol_json.ast", "r") as f:
+                    ast = json.load(f)
                 # NOTE: opcodes file? not in use at present
 
                 if contract_name in contract_names:
                     raise RuntimeError(f"contract {output_filename} appear many times")
 
-                self.artifacts[contract_name] = Artifact(rtbc, initbc, abi)
+                self.artifacts[contract_name] = Artifact(rtbc, initbc, abi, ast)
                 
                 cnwps.add(cnwp)
                     
